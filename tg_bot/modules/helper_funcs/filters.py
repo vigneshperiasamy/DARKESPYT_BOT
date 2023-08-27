8b74cb8c43b8dbca0c06543dfb3b3f88e23b6e5b
Chocoboy-310 <83265686+Chocoboy-310@users.noreply.github.com>
2023-08-27 21:54:12 +0530

    Add files via upload

--- tg_bot/modules/helper_funcs/filters.py
+++ tg_bot/modules/helper_funcs/filters.py
@@ -32,4 +32,3 @@ class CustomFilters(object):
             return bool(message.text or message.sticker or message.photo or message.document or message.video)
 
     has_text = _HasText()
-                   
