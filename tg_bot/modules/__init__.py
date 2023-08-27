6e280dffdf52c1a1157d6246073f509be61e0ffc
Chocoboy-310 <83265686+Chocoboy-310@users.noreply.github.com>
2023-08-27 21:51:01 +0530

    Add files via upload

--- tg_bot/modules/__init__.py
+++ tg_bot/modules/__init__.py
@@ -32,4 +32,3 @@ def __list_all_modules():
 ALL_MODULES = sorted(__list_all_modules())
 LOGGER.info("Modules to load: %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
-                             
